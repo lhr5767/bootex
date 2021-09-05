66415bc3982ccdb5cbcda2cacd3e3166e22d92a8
lhr5767 <ghkfuf789@naver.com>
2021-09-05 19:10:58 +0900

    api 버전관리

--- bootinflearn/src/main/java/com/example/apilec/domain/UserV2.java
+++ bootinflearn/src/main/java/com/example/apilec/domain/UserV2.java
@@ -13,18 +13,7 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonFilter("UserInfo")
-public class User {
-    private Integer id;
-
-    @Size(min = 2,message = "두글자 이상만 가능합니다")
-    private String name;
-    @Past
-    private Date joinDate;
-
-    @JsonIgnore //데이터 제어
-    private String password;
-
-    private String ssn;
-
+@JsonFilter("UserInfoV2")
+public class UserV2 extends User{
+    private String grade;
 }
